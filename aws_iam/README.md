ce22eeea57cb948d41d8e07de3f5645b7b525108
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2025-02-21 19:51:43 +0200

    clean up adding automation user and group

--- aws_iam/README.md
+++ aws_iam/README.md
@@ -6,3 +6,7 @@ Example terraform IaC to create users, roles and attach policies to users, and a
 ### Users
 - Create a user called automation
 - Attach the default policies that aws does, like ec2 full access and iam change password to it
+
+## Groups
+- Create a group called automation_group
+- Attach additional aws policies like s3 full access, autoscaling full access, and others
--- aws_iam/iam_user.tf
+++ aws_iam/iam_user.tf
@@ -2,6 +2,7 @@
 
 resource "aws_iam_user" "automation" {
   name = "automation"
+  path = "/system/"
   permissions_boundary = "arn:aws:iam::aws:policy/AdministratorAccess"
   force_destroy = "true"
 
@@ -14,12 +15,39 @@ resource "aws_iam_access_key" "automation" {
   user = aws_iam_user.automation.name
 }
 
-resource "aws_iam_user_policy_attachment" "attach_IAM_password_policy" {
-  user  = aws_iam_user.automation.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+locals {
+  default_policy_arns = [
+    "arn:aws:iam::aws:policy/IAMUserChangePassword", 
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    ]
+
+  group_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess", 
+    "arn:aws:iam::aws:policy/AutoScalingFullAccess", 
+    "arn:aws:iam::aws:policy/AWSLambda_FullAccess", 
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+    ]
 }
 
-resource "aws_iam_user_policy_attachment" "attach_EC2_full_access_policy" {
+resource "aws_iam_user_policy_attachment" "attach_default_policies" {
+  for_each = toset(local.default_policy_arns)
+
   user  = aws_iam_user.automation.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  policy_arn = each.value
+}
+
+# Create an IAM Group for the automation user.
+
+resource "aws_iam_group" "automation_group" {
+  name = "automation_group"
+  path = "/system/"
+}
+
+# Attach additional policies to the automation group, those ones that are not safe to be attached to a user exclusively.
+
+resource "aws_iam_group_policy_attachment" "attach_group_policies" {
+  for_each = toset(local.group_policy_arns)
+
+  group  = aws_iam_group.automation_group.name
+  policy_arn = each.value
 }
