9bb982ac8c7ac65417f7e5ee781ae30b061d2010
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2025-02-26 01:22:13 +0200

    enabling workflows on all dirs

--- aws_iam/iam_user.tf
+++ aws_iam/iam_user.tf
@@ -1,10 +1,10 @@
 # Create an IAM automation user with the default policies attached to it.
 
 resource "aws_iam_user" "automation" {
-  name = "automation"
-  path = "/system/"
+  name                 = "automation"
+  path                 = "/system/"
   permissions_boundary = "arn:aws:iam::aws:policy/AdministratorAccess"
-  force_destroy = "true"
+  force_destroy        = "true"
 
   tags = {
     Name = "automation_user"
@@ -17,22 +17,22 @@ resource "aws_iam_access_key" "automation" {
 
 locals {
   default_policy_arns = [
-    "arn:aws:iam::aws:policy/IAMUserChangePassword", 
+    "arn:aws:iam::aws:policy/IAMUserChangePassword",
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-    ]
+  ]
 
   group_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess", 
-    "arn:aws:iam::aws:policy/AutoScalingFullAccess", 
-    "arn:aws:iam::aws:policy/AWSLambda_FullAccess", 
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AutoScalingFullAccess",
+    "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
-    ]
+  ]
 }
 
 resource "aws_iam_user_policy_attachment" "attach_default_policies" {
   for_each = toset(local.default_policy_arns)
 
-  user  = aws_iam_user.automation.name
+  user       = aws_iam_user.automation.name
   policy_arn = each.value
 }
 
@@ -48,6 +48,6 @@ resource "aws_iam_group" "automation_group" {
 resource "aws_iam_group_policy_attachment" "attach_group_policies" {
   for_each = toset(local.group_policy_arns)
 
-  group  = aws_iam_group.automation_group.name
+  group      = aws_iam_group.automation_group.name
   policy_arn = each.value
 }
